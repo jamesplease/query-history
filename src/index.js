d658322adfd6872d70d32de33d175a74e0760fe3
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-24 15:30:00 -0700

    Support more options when creating query-history

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import { createBrowserHistory } from 'history';
 import queryString from 'query-string';
 import historyWithQuery from './history-with-query';
 
-export default function createHistoryWithQuery(basename, { arrayFormat } = {}) {
+export default function createHistoryWithQuery(options = {}) {
+  const { queryStringOptions, ...rest } = options;
+
   return historyWithQuery(
-    createBrowserHistory({
-      basename,
-    }),
-    val => queryString.stringify(val, { arrayFormat }),
-    val => queryString.parse(val, { arrayFormat })
+    createBrowserHistory(rest),
+    (val) => queryString.stringify(val, queryStringOptions),
+    (val) => queryString.parse(val, queryStringOptions)
   );
 }
