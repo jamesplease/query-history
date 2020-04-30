a8733dbf84c954c58e851d873258d124b5d9fc20
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-30 10:18:06 -0700

    Split out queryString options

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import queryString from 'query-string';
 import historyWithQuery from './history-with-query';
 
 export default function createHistoryWithQuery(options = {}) {
-  const { queryStringOptions, ...rest } = options;
+  const { stringifyOptions, parseOptions, ...rest } = options;
 
   return historyWithQuery(
     createBrowserHistory(rest),
-    (val) => queryString.stringify(val, queryStringOptions),
-    (val) => queryString.parse(val, queryStringOptions)
+    (val) => queryString.stringify(val, stringifyOptions),
+    (val) => queryString.parse(val, parseOptions)
   );
 }
